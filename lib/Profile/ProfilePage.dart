e12f17631c530a8c1c65e6dd8a6224eccd46abe0
melikesoygullucu <83502252+melikesoygullucu@users.noreply.github.com>
2023-01-12 00:11:52 +0300

    Update ProfilePage.dart

--- lib/Profile/ProfilePage.dart
+++ lib/Profile/ProfilePage.dart
@@ -1,354 +1,204 @@
 import 'package:firebase_auth/firebase_auth.dart';
-
-import 'SettingsPage.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import '../user.dart';
 import 'SideBarPage.dart';
+import 'dart:io';
+import 'SettingsPage.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    String uid = FirebaseAuth.instance.currentUser!.uid;
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'MoMovie',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const ProfileScreen(),
-        //'/tobewatch':(context)=> const ToBeWatchScreen(),
-        //WatchedScreen.routeName:(context)=> const WatchedScreen(),
-      },
-      builder: (context, child) {
-        return _MoMovie(
-          navigator: (child!.key as GlobalKey<NavigatorState>),
-          child: child,
-        );
-      },
-    );
-  }
-}
-
-class _MoMovie extends StatefulWidget {
-  const _MoMovie({
-    Key? key,
-    required this.navigator,
-    required this.child,
-  }) : super(key: key);
-
-  final GlobalKey<NavigatorState> navigator;
-  final Widget child;
-
-  @override
-  State<_MoMovie> createState() => _MoMovieState();
-}
-
-class _MoMovieState extends State<_MoMovie> {
-  @override
-  Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
-
-    return Scaffold(
-        backgroundColor: const Color(0xFFF5EDDC),
-        body: Row(
-          children: [
-            SideBar(
-              width: width,
-              height: height,
-            ),
-            Expanded(
-              child: widget.child,
-            ),
-          ],
-        ));
-  }
-}
-
-class ProfileScreen extends StatelessWidget {
-  static var routeName;
-
+class ProfileScreen extends StatefulWidget {
+  //Home:ProfileScreen
   const ProfileScreen({Key? key}) : super(key: key);
 
-  // static const routeName: '/profile';
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  Future<void> userInfo() async {
+    var dataUser = DataUser().userData;
+    var ref = FirebaseDatabase.instance.ref("users");
+    List infoUser = ref.child(dataUser).get() as List;
+    String userName = infoUser[0];
+  }
+
+  String? email = FirebaseAuth.instance.currentUser!.email.toString();
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
-
-    return Scaffold(
-        body: new Stack(
-      children: <Widget>[
-        ClipPath(
-          child: Container(color: Colors.black.withOpacity(0.8)),
-          clipper: getClipper(),
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios_new_rounded,
+            color: Colors.white70,
+          ),
+          onPressed: () {}, //önceki sayfaya dönecek
         ),
-        Positioned(
-            width: 350.0,
-            top: MediaQuery.of(context).size.height / 5,
-            child: Column(
-              children: <Widget>[
-                Container(
-                    width: 150.0,
-                    height: 150.0,
-                    decoration: BoxDecoration(
-                        color: Colors.red,
-                        image: DecorationImage(
-                            image: NetworkImage(''), fit: BoxFit.cover),
-                        borderRadius: BorderRadius.all(Radius.circular(75.0)),
-                        boxShadow: [
-                          BoxShadow(blurRadius: 7.0, color: Colors.black)
-                        ])),
-                SizedBox(height: 90.0),
-                Text(
-                  "uid",
-                  style: TextStyle(
-                      fontSize: 30.0,
+        backgroundColor: Colors.black87,
+        centerTitle: true,
+        elevation: 0,
+        title: Center(child: const Text('Profile')),
+      ),
+      body: Container(
+          color: Colors.black,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                  colors: [
+                    Colors.white10,
+                    Colors.black54,
+                  ],
+                )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 150,
+                ),
+                child: Column(children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 150,
+                        height: 150,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.white70, width: 2.0),
+                        ),
+                        padding: EdgeInsets.all(8.0),
+                        child: CircleAvatar(
+                          backgroundImage:
+                              AssetImage("assets/avatars/avatar1.png"),
+                          backgroundColor: Colors.blueGrey,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    email!,
+                    style: TextStyle(
+                      color: Colors.white70,
                       fontWeight: FontWeight.bold,
-                      fontFamily: 'Montserrat'),
+                      fontSize: 30.0,
+                    ),
+                  ),
+
+                  // const SizedBox(height: 90,),
+                  // Text("welcome back name", style: TextStyle(color: Colors.white54,
+                  // fontWeight: FontWeight.w300, fontSize: 35),
+                  // ),
+                  // SizedBox(height: 10,),
+                  // const Text("Let's find a movie for you!",
+                  //   style: TextStyle(fontSize: 25, fontWeight: FontWeight.w300,color:Colors.white54),
+                  // ),
+                  // Column(
+                  //   children: [
+                  //     IconButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=> SettingPageUI())); }, icon: const Icon(Icons.movie_filter_outlined,size: 100,color:Colors.red),
+                  //     ),
+                  //   ],
+                  // )
+                ]),
+              ),
+              Align(
+                alignment: const Alignment(1.3, -1.1),
+                child: Container(
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.white30, width: 2.0),
+                  ),
+                  padding: EdgeInsets.all(8.0),
+                  child: IconButton(
+                    icon: Icon(Icons.settings_sharp),
+                    color: Colors.white70,
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SettingPageUI()));
+                    },
+                  ),
                 ),
-                SizedBox(height: 15.0),
-                Text(
-                  'About me',
-                  style: TextStyle(
-                      fontSize: 17.0,
-                      fontStyle: FontStyle.italic,
-                      fontFamily: 'Montserrat'),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 400),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(20.0),
+                    topLeft: Radius.circular(20.0),
+                  ),
                 ),
-                SizedBox(height: 25.0),
-                Container(
-                    height: 30.0,
-                    width: 95.0,
-                    child: Material(
-                      borderRadius: BorderRadius.circular(20.0),
-                      shadowColor: Colors.redAccent,
-                      color: Colors.red,
-                      elevation: 7.0,
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: Center(
-                          child: Text(
-                            'Edit my profile',
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      child: Column(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.movie_filter_outlined,
+                                size: 50, color: Colors.red),
+                          ),
+                          SizedBox(height: 10),
+//random movie
+                          const Text(
+                            'Movies For you!',
                             style: TextStyle(
-                                color: Colors.white, fontFamily: 'Montserrat'),
+                              fontSize: 25,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.star_rate_outlined,
+                              size: 50, color: Colors.red),
+                        ),
+                        SizedBox(height: 10),
+                        const Text(
+                          'TOP 20',
+                          style: TextStyle(
+                            fontSize: 25,
                           ),
                         ),
-                      ),
-                    )),
-                SizedBox(height: 25.0),
-                Container(
-                    height: 30.0,
-                    width: 95.0,
-                    child: Material(
-                      borderRadius: BorderRadius.circular(20.0),
-                      shadowColor: Colors.redAccent,
-                      color: Colors.red,
-                      elevation: 7.0,
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => SettingPageUI()));
-                        },
-                        child: Center(
-                          child: Text(
-                            'Settings',
-                            style: TextStyle(
-                                color: Colors.white, fontFamily: 'Montserrat'),
-                          ),
-                        ),
-                      ),
-                    ))
-              ],
-            ))
-      ],
+                        Container(
+                            // child: const ProfilTasarimi(),
+                            )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )),
     ));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-        body: new Stack(
-      children: <Widget>[
-        ClipPath(
-          child: Container(color: Colors.black.withOpacity(0.8)),
-          clipper: getClipper(),
-        ),
-        Positioned(
-            width: 350.0,
-            top: MediaQuery.of(context).size.height / 5,
-            child: Column(
-              children: <Widget>[
-                Container(
-                    width: 150.0,
-                    height: 150.0,
-                    decoration: BoxDecoration(
-                        color: Colors.red,
-                        image: DecorationImage(
-                            image: NetworkImage(''), fit: BoxFit.cover),
-                        borderRadius: BorderRadius.all(Radius.circular(75.0)),
-                        boxShadow: [
-                          BoxShadow(blurRadius: 7.0, color: Colors.black)
-                        ])),
-                SizedBox(height: 90.0),
-                Text(
-                  'Name',
-                  style: TextStyle(
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Montserrat'),
-                ),
-                SizedBox(height: 15.0),
-                Text(
-                  'About me',
-                  style: TextStyle(
-                      fontSize: 17.0,
-                      fontStyle: FontStyle.italic,
-                      fontFamily: 'Montserrat'),
-                ),
-                SizedBox(height: 25.0),
-                Container(
-                    height: 30.0,
-                    width: 95.0,
-                    child: Material(
-                      borderRadius: BorderRadius.circular(20.0),
-                      shadowColor: Colors.redAccent,
-                      color: Colors.red,
-                      elevation: 7.0,
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: Center(
-                          child: Text(
-                            'Edit my profile',
-                            style: TextStyle(
-                                color: Colors.white, fontFamily: 'Montserrat'),
-                          ),
-                        ),
-                      ),
-                    )),
-                SizedBox(height: 25.0),
-                Container(
-                    height: 30.0,
-                    width: 95.0,
-                    child: Material(
-                      borderRadius: BorderRadius.circular(20.0),
-                      shadowColor: Colors.redAccent,
-                      color: Colors.red,
-                      elevation: 7.0,
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: Center(
-                          child: Text(
-                            'Log out',
-                            style: TextStyle(
-                                color: Colors.white, fontFamily: 'Montserrat'),
-                          ),
-                        ),
-                      ),
-                    ))
-              ],
-            ))
-      ],
-    ));
-  }
-}
-
-class getClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    var path = new Path();
-
-    path.lineTo(0.0, size.height / 1.9);
-    path.lineTo(size.width + 125, 0.0);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) {
-    // TODO: implement shouldReclip
-    return true;
-  }
-}
-// class SideBar extends StatefulWidget {
-//   const SideBar({Key? key,
-//   required this.height,
-//   required this.width,
-//  // required this.navigator,
-//    }) : super(key: key);
-//
-//   final double height;
-//   final double width;
-//  // final GlobalKey<NavigatorState> navigator;
+// class ProfilTasarimi extends StatefulWidget {
+//   const ProfilTasarimi({Key? key}) : super(key: key);
 //
 //   @override
-//   State<SideBar> createState() => _SideBarState();
+//   State<ProfilTasarimi> createState() => _ProfilTasarimiState();
 // }
 //
-// class _SideBarState extends State<SideBar> {
-//   List<Map> menu =[
-//     {'title': 'Profile','routeName': '/profile'},
-//     {'title': 'To Be Watch','routeName': '/tobewatch'},
-//     {'title': 'Watched','routeName': '/watched'},
-//   ];
+// class _ProfilTasarimiState extends State<ProfilTasarimi> {
+//   late File yuklenecekDosya;
+//   FirebaseAuth auth = FirebaseAuth.instance;
+//   // FirebaseStorage storage = FirebaseStorage.instance;
+//   late String indirmeBaglantisi;
 //
-//   int sideBarIndex =0;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       width: widget.width*0.2,
-//       color: Colors.deepPurple,
-//       child: Column(children: [
-//         SizedBox(height: widget.height*0.05),
-//
-//         ListView.builder(
-//             shrinkWrap: true,
-//             itemCount: menu.length,
-//             physics: const NeverScrollableScrollPhysics(),
-//             itemBuilder: (context,index){
-//               return RotatedBox(
-//                    quarterTurns: 3,
-//                    child: TextButton(
-//                     onPressed: (){
-//                       setState(() {
-//                         sideBarIndex=index;
-//                       });
-//
-//                     },
-//                      style: TextButton.styleFrom(minimumSize: const Size(200,50)),
-//                     child: Text(
-//                 menu[index]['title'],
-//                       style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-//                         fontWeight: (index== sideBarIndex)
-//                             ?FontWeight.bold
-//                             :FontWeight.normal,
-//                         color: (index==sideBarIndex)
-//                             ?Colors.white
-//                             :Colors.white.withAlpha(200),
-//                         letterSpacing: 2,
-//                       ),
-//                 ),
-//                 ),
-//               );
-//             },
-//         ),]
-//         ),
-//     );
-//   }
 // }
-//
-//
